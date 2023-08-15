0cf1ad244ee0a587070de3888fbf020300bfe0a8
Haden MV <med4en@gmail.com>
2023-08-15 00:47:01 +0000

    Book somthing hot fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -61,7 +61,7 @@ class PostController extends Controller
      */
     public function lessons($id)
     {
-        return Post::get()->where('post_type', 'lesson')->where('post_parent', $id)->first();
+        return Post::get()->where('post_type', 'lesson')->where('post_parent', $id);
     }
     public function topics($id)
     {
